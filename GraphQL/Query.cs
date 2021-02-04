62bfa9b7fb8bff6e94ac2fd619c3dfc38e52ed7b
Justin Eder <justineder@gmail.com>
2021-02-04 12:59:47 -0600

    Adding DataLoader

--- GraphQL/Query.cs
+++ GraphQL/Query.cs
@@ -3,7 +3,8 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using ConferencePlanner.GraphQL.Data;
 using HotChocolate;
-
+using ConferencePlanner.GraphQL.DataLoader;
+using System.Threading;
 
 namespace ConferencePlanner.GraphQL
 {
@@ -12,5 +13,10 @@ namespace ConferencePlanner.GraphQL
     [UseApplicationDbContext]
     public Task<List<Speaker>> GetSpeakers([ScopedService] ApplicationDbContext context) =>
         context.Speakers.ToListAsync();
+    public Task<Speaker> GetSpeakerAsync(
+      int id,
+      SpeakerByIdDataLoader dataLoader,
+      CancellationToken cancellationToken) =>
+      dataLoader.LoadAsync(id, cancellationToken);
   }
 }
