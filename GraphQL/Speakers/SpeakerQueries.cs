ede30c74d684c564d033c6d9b84d2689f336a511
Justin Eder <justineder@gmail.com>
2021-02-04 17:15:09 -0600

    Build out the schema

--- GraphQL/Speakers/SpeakerQueries.cs
+++ GraphQL/Speakers/SpeakerQueries.cs
@@ -1,23 +1,26 @@
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using ConferencePlanner.GraphQL.Data;
-using HotChocolate;
 using ConferencePlanner.GraphQL.DataLoader;
-using System.Threading;
+using HotChocolate;
+using HotChocolate.Types;
 using HotChocolate.Types.Relay;
 
-namespace ConferencePlanner.GraphQL
+namespace ConferencePlanner.GraphQL.Speakers
 {
-  public class Query
+  [ExtendObjectType(Name = "Query")]
+  public class SpeakerQueries
   {
     [UseApplicationDbContext]
     public Task<List<Speaker>> GetSpeakers([ScopedService] ApplicationDbContext context) =>
         context.Speakers.ToListAsync();
+
     public Task<Speaker> GetSpeakerAsync(
-      [ID(nameof(Speaker))] int id,
-      SpeakerByIdDataLoader dataLoader,
-      CancellationToken cancellationToken) =>
-      dataLoader.LoadAsync(id, cancellationToken);
+        [ID(nameof(Speaker))] int id,
+        SpeakerByIdDataLoader dataLoader,
+        CancellationToken cancellationToken) =>
+        dataLoader.LoadAsync(id, cancellationToken);
   }
 }
