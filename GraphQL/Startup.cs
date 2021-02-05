28dee1883fd8fb68e568dab8aa1c23fdbb45e733
Justin Eder <justineder@gmail.com>
2021-02-05 14:00:22 -0600

    Think beyond CRUD

--- GraphQL/Startup.cs
+++ GraphQL/Startup.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using ConferencePlanner.GraphQL;
 using ConferencePlanner.GraphQL.Data;
 using ConferencePlanner.GraphQL.DataLoader;
+using ConferencePlanner.GraphQL.Sessions;
 using ConferencePlanner.GraphQL.Speakers;
+using ConferencePlanner.GraphQL.Tracks;
 using ConferencePlanner.GraphQL.Types;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -28,7 +30,9 @@ namespace GraphQL
         .AddQueryType(d => d.Name("Query"))
           .AddTypeExtension<SpeakerQueries>()
         .AddMutationType(d => d.Name("Mutation"))
+          .AddTypeExtension<SessionMutations>()
           .AddTypeExtension<SpeakerMutations>()
+          .AddTypeExtension<TrackMutations>()
         .AddType<AttendeeType>()
         .AddType<SessionType>()
         .AddType<SpeakerType>()
